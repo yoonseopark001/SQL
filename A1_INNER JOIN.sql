272ed311500ab48667d8d148f70a9145cee1d582
Yoonseo Park <yoonseopark001@gmail.com>
2020-01-22 10:22:15 +0900

    Update A1_INNER JOIN.sql

--- A1_INNER JOIN.sql
+++ A1_INNER JOIN.sql
@@ -32,3 +32,13 @@ SELECT cities.name as city, countries.name as country, region
 FROM cities
   INNER JOIN countries
     ON cities.country_code = countries.code;
+
+------------------------------------
+
+-- 3. Select fields with aliases
+SELECT c.code AS country_code, c.name, e.year, e.inflation_rate
+FROM countries AS c
+  -- 1. Join to economies (alias e)
+  INNER JOIN economies AS e
+    -- 2. Match on code
+    ON c.code = e.code;
