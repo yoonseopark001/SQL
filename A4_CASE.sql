fc2a881ca6a2733f0c727f702a05acb73ebc9c0a
Yoonseo Park <yoonseopark001@gmail.com>
2020-02-06 23:23:44 +0900

    Update A4_CASE.sql

--- A4_CASE.sql
+++ A4_CASE.sql
@@ -7,3 +7,76 @@ SELECT name, continent, indep_year,
 FROM states
 ORDER BY indep_year_group
 
+--------------------------------------------------
+
+SELECT name, continent, code, surface_area,
+    -- 1. First case
+    CASE WHEN surface_area > 2000000 THEN 'large'
+        -- 2. Second case
+        WHEN surface_area < 2000000 AND surface_area > 350000 THEN 'medium'
+        -- 3. Else clause + end
+        ELSE 'small' END
+        -- 4. Alias name
+        AS geosize_group
+-- 5. From table
+FROM countries;
+
+-----------------------
+
+SELECT name, continent, code, surface_area,
+    CASE WHEN surface_area > 2000000
+            THEN 'large'
+       WHEN surface_area > 350000
+            THEN 'medium'
+       ELSE 'small' END
+       AS geosize_group
+INTO countries_plus
+FROM countries;
+
+--------------------------------------------------
+
+SELECT country_code, size,
+    -- 1. First case
+    CASE WHEN size > 50000000 THEN 'large'
+        -- 2. Second case
+        WHEN size > 1000000 THEN 'medium'
+        -- 3. Else clause + end
+        ELSE 'small' END
+        -- 4. Alias name (popsize_group)
+        AS popsize_group
+-- 5. From table
+FROM populations
+-- 6. Focus on 2015
+WHERE year = 2015;
+
+--------------------------------------------------
+# INTO
+
+SELECT name, continent, code, surface_area,
+    CASE WHEN surface_area > 2000000
+            THEN 'large'
+       WHEN surface_area > 350000
+            THEN 'medium'
+       ELSE 'small' END
+       AS geosize_group
+INTO countries_plus
+FROM countries;
+
+--------------------------------------------------
+
+SELECT country_code, size,
+    CASE WHEN size > 50000000 THEN 'large'
+        WHEN size > 1000000 THEN 'medium'
+        ELSE 'small' END
+        AS popsize_group
+-- 1. Into table
+INTO pop_plus
+FROM populations
+WHERE year = 2015;
+
+-- 2. Select all columns of pop_plus
+SELECT * FROM pop_plus;
+
+--------------------------------------------------
+
+
