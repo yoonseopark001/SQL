529f2dbe4f1b92833c1c6c8ee062fd6cb26d4cf1
Yoonseo Park <yoonseopark001@gmail.com>
2020-02-06 23:27:41 +0900

    Update A4_CASE.sql

--- A4_CASE.sql
+++ A4_CASE.sql
@@ -79,4 +79,25 @@ SELECT * FROM pop_plus;
 
 --------------------------------------------------
 
+SELECT country_code, size,
+  CASE WHEN size > 50000000
+            THEN 'large'
+       WHEN size > 1000000
+            THEN 'medium'
+       ELSE 'small' END
+       AS popsize_group
+INTO pop_plus       
+FROM populations
+WHERE year = 2015;
+
+-- 5. Select fields
+SELECT name, continent, geosize_group, popsize_group
+-- 1. From countries_plus (alias as c)
+FROM countries_plus AS c
+  -- 2. Join to pop_plus (alias as p)
+  JOIN pop_plus AS p
+    -- 3. Match on country code
+    ON p.country_code = c.code
+-- 4. Order the table    
+ORDER BY geosize_group;
 
